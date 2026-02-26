600b8d7d5135ba88f671e27a250a6ad130e0bfa4
yaghtinpouyan <132679813+yaghtinpouyan@users.noreply.github.com>
2026-02-26 14:14:05 -0500

    not sure if it works or not but its something

--- src/main/java/frc/robot/test/IntakeTest.java
+++ src/main/java/frc/robot/test/IntakeTest.java
@@ -1,5 +1,32 @@
 package frc.robot.test;
 
-public class IntakeTest {
-    
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.intake;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class IntakeTest extends SubsystemBase {
+    private static IntakeTest intakeTest = null;
+    private intake ballIntake = intake.getInstance();
+    double testIntakePower = SmartDashboard.getNumber("Intake Power", 0);
+
+    public void setTestIntake(boolean runTest) {
+        testIntakePower = SmartDashboard.getNumber("Intake Power", 0);
+
+        if (runTest) {
+            ballIntake.setIntakePivot();
+            if (testIntakePower >= 0) ballIntake.runIntake(true);
+            else ballIntake.runIntake(false);
+            ballIntake.runHopper(true);
+        } else {
+            ballIntake.runIntake(false);
+            ballIntake.runHopper(false);
+        }
+    }
+
+    public static IntakeTest getInstance() {
+        if (intakeTest == null) {
+            intakeTest = new IntakeTest();
+        }
+        return intakeTest;
+    }
 }
