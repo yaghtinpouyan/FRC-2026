77e23abd6c14d93e83639a8d8690ff8231a59423
Shoesher <raidertheweeb@gmail.com>
2026-03-18 22:18:41 -0400

    shooter RPM changes

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,14 +1,11 @@
 package frc.robot.subsystems;
 
 import static edu.wpi.first.units.Units.Amps;
-import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.RPM;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
 import static edu.wpi.first.units.Units.RotationsPerSecondPerSecond;
 import static edu.wpi.first.units.Units.Seconds;
-import static edu.wpi.first.units.Units.Volts;
 
-import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
@@ -16,19 +13,14 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.units.measure.AngularVelocity;
-import edu.wpi.first.units.measure.Voltage;
-import edu.wpi.first.wpilibj.DutyCycle;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.idConstants;
-import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
 import yams.gearing.GearBox;
 import yams.gearing.MechanismGearing;
-import yams.mechanisms.config.ArmConfig;
 import yams.motorcontrollers.SmartMotorController;
 import yams.motorcontrollers.SmartMotorControllerConfig;
 import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
 import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
-import yams.motorcontrollers.local.SparkWrapper;
 import yams.motorcontrollers.remote.TalonFXWrapper;
 
 public class Intake extends SubsystemBase{
@@ -38,11 +30,7 @@ public class Intake extends SubsystemBase{
     TalonFX indexer;
     SparkMax pivotMotor;
     private final SmartMotorControllerConfig falconConfig;
-    // private final SmartMotorControllerConfig neoConfig;
     private SmartMotorController indexerSystem;
-    // private SmartMotorController pivotingSystem;
-    // private ArmConfig pivotConfig;
-
     
     private Intake(){
         intakeRollers = new TalonFX(idConstants.falcon500_I1);
@@ -61,27 +49,7 @@ public class Intake extends SubsystemBase{
                             .withSimFeedforward(new SimpleMotorFeedforward(0.27937, 0.089836, 0.014557))
                             .withControlMode(ControlMode.CLOSED_LOOP);
 
-        //  neoConfig = new SmartMotorControllerConfig(this)
-        //                 .withClosedLoopController(0.00016541, 0, 0, RPM.of(5500), RotationsPerSecondPerSecond.of(80))
-        //                 .withGearing(new MechanismGearing(GearBox.fromReductionStages(47.32, 1)))
-        //                 .withIdleMode(MotorMode.COAST)
-        //                 .withStatorCurrentLimit(Amps.of(20))
-        //                 .withMotorInverted(true)
-        //                 .withClosedLoopRampRate(Seconds.of(0.25))
-        //                 .withOpenLoopRampRate(Seconds.of(0.25))
-        //                 .withFeedforward(new SimpleMotorFeedforward(0.27937, 0.089836, 0.014557))
-        //                 .withSimFeedforward(new SimpleMotorFeedforward(0.27937, 0.089836, 0.014557))
-        //                 .withControlMode(ControlMode.CLOSED_LOOP);
-
         indexerSystem = new TalonFXWrapper(indexer, DCMotor.getFalcon500(1), falconConfig);
-        // pivotingSystem = new SparkWrapper(pivotMotor, DCMotor.getNEO(1), neoConfig);
-        // pivotConfig = new ArmConfig(pivotingSystem)
-        //                    .withLength(Meters.of(0.3366))
-        //                    .withSoftLimits(Degrees.of(26.5), Degrees.of(152))
-        //                    .withHardLimit(Degrees.of(26.5), Degrees.of(152))
-        //                    .withTelemetry("Pivot", TelemetryVerbosity.HIGH)
-        //                    .withMass(Kilograms.of(2.714))
-        //                    .withStartingPosition(Degrees.of(0));
     }
 
     public void runIndexer(){
@@ -102,13 +70,6 @@ public class Intake extends SubsystemBase{
             intakeRollers.setVoltage(0);
         }
     }
-
-    // public void setPivotAngle(int pov){
-    //     if(pov == 0) pivot.setPosition(Degrees.of(0));
-    //     else if(pov == 180){
-    //         pivotingSystem.setPosition(Degrees.of(123));
-    //     }
-    // }
 
     public void setPivot(int pov){
         if(pov == 0) pivotMotor.setVoltage(2.4);
--- src/main/java/frc/robot/subsystems/OperatorInterface.java
+++ src/main/java/frc/robot/subsystems/OperatorInterface.java
@@ -12,9 +12,7 @@ public class OperatorInterface extends SubsystemBase{
     private Vision vision = Vision.getInstance();
     private Intake ballIntake = Intake.getInstance();
     private Shooter ballShooter = Shooter.getInstance();
-    // private Climb climber = Climb.getInstance();
     private Telemetry telemetry = Telemetry.getInstance();
-    //private ShooterTest shooterTest = ShooterTest.getInstance();
 
     private OperatorInterface(){
         controller1 = new XboxController(0);
@@ -22,8 +20,6 @@ public class OperatorInterface extends SubsystemBase{
     }
 
     private void updateDrive(){
-        // drivetrain.driveCommand(() -> -controller1.getRawAxis(1), 
-        //     () -> -controller1.getRawAxis(0), () -> -controller1.getRawAxis(4));
         drivetrain.driveInputHandler(() -> -controller1.getRawAxis(1), 
             () -> -controller1.getRawAxis(0), () -> -controller1.getRawAxis(4), controller1.getYButton());
     }
@@ -31,7 +27,6 @@ public class OperatorInterface extends SubsystemBase{
     private void updateIntake(){
         ballIntake.runRollers(controller2.getRawAxis(3), controller2.getRawButton(6));
         ballIntake.setPivot(controller2.getPOV());
-
     }
 
     private void updateShooter(){
