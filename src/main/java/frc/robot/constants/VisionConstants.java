f3848add521a3e297eac75fa31b5240d8370703c
RichiEee3704 <ya.richard23@gmail.com>
2026-03-20 21:36:12 -0400

    hi vision cam 2 idk anymore bye

--- src/main/java/frc/robot/constants/VisionConstants.java
+++ src/main/java/frc/robot/constants/VisionConstants.java
@@ -6,10 +6,18 @@ import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
 
 public class VisionConstants {
-    public static final double camPosX = -0.0407;
-    public static final double camPosY = -0.2065;
-    public static final double camPosZ = 0.38372;
-    public static final double camRotPitch = 45;
+    //Camera 1 position and rotation relative to the robot center (in meters and degrees)
+    public static final double camPosX = 0.2096;
+    public static final double camPosY = -0.028653;
+    public static final double camPosZ = -0.506743;
+    public static final double camRotPitch = 15;
+
+    //Camera 2 position and rotation relative to the robot center (in meters and degrees)
+    public static final double cam2PosX = -0.209550;
+    public static final double cam2PosY = -0.10808;
+    public static final double cam2PosZ = -0.505474;
+    public static final double cam2RotPitch = 15;
+
 
     // The standard deviations of our vision estimated poses, which affect correction rate
     // (Fake values. Experiment and determine estimation noise on an actual robot.)
