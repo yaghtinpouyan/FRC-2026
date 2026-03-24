90338e26737c10ab6c15ceb77d5a3309afe1d56f
Pooyan <mail>
2026-03-24 12:07:05 -0400

    chc

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -98,7 +98,7 @@ public class Drive extends SubsystemBase
             (speeds, feedforwards) -> drive(speeds), 
             new PPHolonomicDriveController( 
                     new PIDConstants(5.0, 0.0, 0.0), // Movement PID constants
-                    new PIDConstants(0.4, 0.0, 0.01) // Rotation PID constants
+                    new PIDConstants(1, 0.0, 0.01) // Rotation PID constants
             ),
             config,
             () -> {
